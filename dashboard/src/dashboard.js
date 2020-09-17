ec51a21b7cd9c58801ffca926561aa1fba633343
paulo70 <paulofrontend@gmail.com>
2020-09-16 22:42:16 -0300

    Add logic to change graphics values

--- dashboard/src/dashboard.js
+++ dashboard/src/dashboard.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './dashboard.css'
 import Chart from 'react-google-charts'
 
@@ -13,8 +13,30 @@ function Dashboard() {
     ['May', 80],
     ['June', 27]
   ])
+
+  useEffect(() => {
+    function changeGraphics(){
+      const dataGraphics = data.map(line => {
+        if(Number.isInteger(line[1])) {
+          line[1] = Math.floor(Math.random() * 101)
+        }
+
+        return line
+      })
+
+      setData(dataGraphics)
+    }
+
+    const intervalID = setInterval(() => changeGraphics(), 5000)
+
+    return () => {
+      clearInterval(intervalID)
+    }
+
+  },[data])
+ 
   return (
-    <>
+    <div>
       <h1>Dashboard</h1>
       <Chart 
         width={'400px'}
@@ -25,7 +47,69 @@ function Dashboard() {
           title: Title
         }}
       />
-    </>
+
+      <Chart 
+        width={'400px'}
+        height={'300px'}
+        chartType={'PieChart'}
+        data={data}
+        options={{
+          title: Title,
+          is3D: true
+        }}
+      />
+
+      <Chart 
+        width={'400px'}
+        height={'300px'}
+        chartType={'PieChart'}
+        data={data}
+        options={{
+          title: Title,
+          pieHole: 0.4
+        }}
+      />
+
+      <Chart 
+        width={'400px'}
+        height={'300px'}
+        chartType={'BarChart'}
+        data={data}
+        options={{
+          title: Title,
+          chartArea:{ width: '50%' },
+          hAxis:{ title: 'Quantity' },
+          vAxis:{ title: 'Month'},
+          animation:{ duration: 1000, easing: 'out', startup: true}
+        }}
+      />
+
+      <Chart 
+        width={'400px'}
+        height={'300px'}
+        chartType={'LineChart'}
+        data={data}
+        options={{
+          title: Title,
+          hAxis:{ title: 'Month' },
+          vAxis:{ title: 'Quantity'},
+          animation:{ duration: 1000, easing: 'out', startup: true}
+        }}
+      />
+
+      <Chart 
+        width={'400px'}
+        height={'300px'}
+        chartType={'AreaChart'}
+        data={data}
+        options={{
+          title: Title,
+          hAxis:{ title: 'Month' },
+          vAxis:{ title: 'Quantity'},
+          animation:{ duration: 1000, easing: 'out', startup: true}
+        }}
+      />
+    </div>
   )
 }
 
